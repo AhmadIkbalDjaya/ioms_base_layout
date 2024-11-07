38fb4508264152054cf469d45d1f2ec74c7e3216
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2024-11-07 10:37:39 +0800

    update: background gradient and blur

--- lib/base_layout.dart
+++ lib/base_layout.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_testing/navbar.dart';
 import 'package:flutter_testing/sidebar.dart';
@@ -9,49 +11,78 @@ class BaseLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(
-        vertical: 20,
-        horizontal: 20,
-      ),
       decoration: const BoxDecoration(
         image: DecorationImage(
-          image: AssetImage('assets/bg_app.png'),
+          image: AssetImage('assets/bg_app2.png'),
           fit: BoxFit.cover,
         ),
       ),
-      child: Column(
-        children: [
-          const Navbar(),
-          const SizedBox(height: 25),
-          Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Expanded(
-                  flex: 1,
-                  child: SidebarCrewRecruitment(),
-                ),
-                const SizedBox(width: 15),
-                Expanded(
-                  flex: 4,
-                  child: Container(
-                    height: double.maxFinite,
-                    width: double.maxFinite,
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 15,
-                      horizontal: 15,
-                    ),
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(80, 0, 0, 0),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: body,
-                  ),
-                ),
-              ],
-            ),
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          vertical: 20,
+          horizontal: 20,
+        ),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.red.withOpacity(0.6),
+              Colors.transparent,
+            ],
+            begin: Alignment.bottomCenter,
+            end: const Alignment(0, -2),
           ),
-        ],
+        ),
+        child: Column(
+          children: [
+            const Navbar(),
+            const SizedBox(height: 25),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Expanded(
+                    flex: 1,
+                    child: SidebarCrewRecruitment(),
+                  ),
+                  const SizedBox(width: 15),
+                  Expanded(
+                    flex: 4,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(
+                          sigmaX: 1000,
+                          sigmaY: 1000,
+                        ),
+                        child: Container(
+                          height: double.maxFinite,
+                          width: double.maxFinite,
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 15,
+                            horizontal: 15,
+                          ),
+                          decoration: BoxDecoration(
+                            // color: const Color.fromARGB(80, 0, 0, 0),
+                            borderRadius: BorderRadius.circular(20),
+                            gradient: const LinearGradient(
+                              colors: [
+                                Color.fromARGB(150, 0, 0, 0),
+                                Colors.transparent,
+                              ],
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                            ),
+                          ),
+                          child: body,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
